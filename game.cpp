3b27824f25144d53cfc815264b4300b03984381b
Abdul Haq <ahaq20@my.whitworth.edu>
2017-11-30 21:15:58 -0800

    comments added

--- game.cpp
+++ game.cpp
@@ -18,6 +18,7 @@ void hangman() {
 	string word = getWord();
 	int len = word.length();
 	string w = "";
+	//creating word full of '*' of length of the original word
 	for (int i = 0; i < len; i++) {
 		w += '*';
 	}
@@ -27,18 +28,21 @@ void hangman() {
 	int count = 0;
 	while (count < 6) {
 		cout << "\n";
+		//check to see if user won
 		if (w == word) {
 			cout << "CONGRATULAITONS!\n";
 			return;
 		}
 		char choice;
+		//asking for choice
 		cout << "Word: " << w << endl;
 		cout << "Guesses remaining: " << 5 - count << endl;
 		cout << "Enter a letter: ";
 		cin >> choice;
 
 		int check = 0;
-
+		
+		//check to see if the letter entered is in the word
 		for (int i = 0; i < len; i++) {
 			if (word[i] == choice) {
 				check++;
@@ -59,13 +63,17 @@ void hangman() {
 
 string getWord() {
 	ifstream fin;
+	//opening the text file full of words
 	fin.open("dict.txt");
 
+	//list to store all words
 	vector <string> list;
 	string line;
-
+	
+	//storing every word to list
 	while (getline(fin, line))
 		list.push_back(line);
-
+	
+	//randomly choosing a word from that list
 	return list[int(rand()) % list.size()];
 }
